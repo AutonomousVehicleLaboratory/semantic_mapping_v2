7041ce35c25cd0fa99ecce432b34db2292a38fe5
ShashankVenkatramani <cjvshashank@gmail.com>
2023-06-13 17:23:15 -0700

    Failing on catkin build

--- quick_run.sh
+++ quick_run.sh
@@ -7,15 +7,19 @@ sh ./docker/semantic-mapping
 # Run docker container, bind the semantic_mapping_v2 to the src of home/rosws
 docker run --name quick-mapping -it --gpus all -v "$(pwd):/home/rosws/src/semantic_mapping_v2:rw" --rm vision-semantic-segmentation:noetic /bin/bash -c "
 
-# Add map_reduction
+# Add map_reduction and catkin
 cd /home/rosws/src;
-ls;
 git clone https://github.com/AutonomousVehicleLaboratory/map_reduction;
+git clone https://github.com/ros/catkin.git;
+
+# Install catkin_tools
+sudo apt-get update
+sudo apt-get install python3-osrf-pycommon python3-catkin-tools
 
 # Initialize Catkin workspace
 cd /home/rosws/;
-catkin_init_workspace;
+catkin init;
 
 # Make workspace
-catkin_make;
+catkin build;
 "
